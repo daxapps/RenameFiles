e58f9ef6c0273531e55429f7c79aebe7b69aca55
Jason Crawford <daxapps777@gmail.com>
2016-07-25 08:41:39 -0500

    updates file path

--- rename_files.py
+++ rename_files.py
@@ -3,11 +3,11 @@ import re
 
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir("/Users/Dax/Downloads/prank")
+    file_list = os.listdir("/Users/Dax/version-control/RenameFiles/prank")
     #print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is " + saved_path)
-    os.chdir("/Users/Dax/Downloads/prank")
+    os.chdir("/Users/Dax/version-control/RenameFiles/prank")
 
     #(2) for each file, rename filename
     for file_name in file_list:
